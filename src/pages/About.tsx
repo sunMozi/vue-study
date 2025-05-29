7f88ca00521ad2d8eecfe7e20263cdb2d225aa34
mozi <2138844072@qq.com>
2025-05-29 11:18:04 +0800

    format

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,11 @@
-import { defineComponent, ref } from 'vue'
+import { defineComponent, onMounted, ref } from 'vue'
 
 export default defineComponent(() => {
   const message = ref('Welcome to the About Page')
+
+  onMounted(() => {
+    console.log('About Page')
+  })
 
   return () => <div class="text-center text-2xl text-blue-700 font-semibold">{message.value}</div>
 })
